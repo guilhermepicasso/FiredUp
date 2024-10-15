2db4483bbe7a95a3f9785badd1d02b5657b5b49a
AliePonzani <alinef.ponzani@gmail.com>
2024-10-15 20:22:04 -0300

    atualizações de estrutura dos arquivos

---
+++ fired-up/src/Pages/Home/HomeView.js
@@ -0,0 +1,13 @@
+
+
+function HomeView() {
+  return (
+    <div className="HomeView">
+      <section className="Header">
+            <img src="" alt="Logo marca da FiredUp" />
+      </section>
+    </div>
+  );
+}
+
+export default HomeView;
