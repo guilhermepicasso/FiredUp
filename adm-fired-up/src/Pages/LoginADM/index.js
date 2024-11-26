1fba9f29c35c5b89381a1e542cd3d57771ccd23d
Guilherme Picasso <guipicasso3@gmail.com>
2024-11-26 12:20:42 -0300

    Login ADM

---
+++ adm-fired-up/src/Pages/LoginADM/index.js
@@ -0,0 +1,90 @@
+import './index.scss'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { verificacaoLogin } from '../../API/chamadas.js';
+import {useAuth} from '../../components/UserContext/AuthContext.js';
+import { jwtDecode } from "jwt-decode";
+
+
+function LoginADM() {
+    const [ra, setRa] = useState('');
+    const [senha, setSenha] = useState('');
+    const navigate = useNavigate();
+    const { login } = useAuth();
+
+    const handleClick = async () => {
+        if (!ra || !senha) {
+            console.log('teste ')
+            toast.info("Por favor, preencha todos os campos");
+            return;
+        }
+        try {
+            const corpo = {
+                ra: ra,
+                senha: senha
+            };
+            const response = await verificacaoLogin(corpo);
+            const { token } = response.data;
+            console.log(token);
+
+            if (token) {
+                localStorage.setItem('token', token);
+                const decoded = jwtDecode(token);
+                console.log(decoded.infoUsuario);
+                toast.success("Login efetuado com Sucesso");
+                login(decoded.infoUsuario);
+                navigate('/HomeViewADM');
+            }
+        } catch (error) {
+            if (error.response && (error.response.status === 404 || error.response.status === 401)) {
+                console.log(error.response.message);
+                toast.warning("Credenciais inválidas");
+            } else {
+                toast.warning("Erro ao fazer o Login: " + error.message);
+            }
+            console.log(error.message);
+        }
+    };
+
+    return (
+        <section className='login_page'>
+            <div className='Login_container'>
+                <div className='logo'>
+                    <img src='/Assets/images/logoFired.png' alt='Logo da FiredUp' />
+                </div>
+                <div className='LoginSlogan'>
+                    <h1>Bem-vindo</h1>
+                    <span>Entre com as credenciais de acesso do Senac</span>
+                </div>
+                <div className='inputs'>
+                    <label htmlFor="inputRa">RA de Matrícula:</label>
+                    <input
+                        type="number"
+                        id="inputRa"
+                        value={ra}
+                        onChange={e => setRa(e.target.value)}
+                    />
+                </div>
+                <div className='inputs'>
+                    <label htmlFor="inputName">Insira seu nome:</label>
+                    <input
+                        type="text"
+                        id="inputName"
+                        value={senha}
+                        onChange={e => setSenha(e.target.value)}
+                    />
+                </div>
+                <div onClick={handleClick} className="login_link">
+                    <span>Entrar</span>
+                </div>
+            </div>
+            <div className='Login_banner'>
+                <h1>Centro Esportivo</h1>
+                <img className='banner' src='/Assets/images/logoBanner.png' alt='Banner Login' />
+            </div>
+        </section>
+    );
+}
+
+export default LoginADM;
--- adm-fired-up/src/index.js
+++ adm-fired-up/src/index.js
@@ -1,9 +1,11 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { AuthProvider } from './components/UserContext/AuthContext';
 
 import App from './Pages/App';
 import Reservas from './Pages/Reservas';
@@ -16,25 +18,29 @@ import HomeViewADM from './Pages/HomeAdm';
 import GerenciarReservas from './Pages/GerenciarReservas';
 import GerenciarEspacos from './Pages/GerenciarEspacos';
 import PDFViewer from './components/Footer/PDFViewer';
+import LoginADM from './Pages/LoginADM';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/TermoDeUso' element={<PDFViewer />} />
-        <Route path='/HomeViewADM' element={<HomeViewADM />} />
-        <Route path='/GerenciarReservas' element={<GerenciarReservas />} />
-        <Route path='/GerenciarEspacos' element={<GerenciarEspacos />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginADM />} />
+          <Route path='/App' element={<App />} />
+          <Route path='/TermoDeUso' element={<PDFViewer />} />
+          <Route path='/HomeViewADM' element={<HomeViewADM />} />
+          <Route path='/GerenciarReservas' element={<GerenciarReservas />} />
+          <Route path='/GerenciarEspacos' element={<GerenciarEspacos />} />
 
-        <Route path='/Espaco' element={<Espaco />} />
-        <Route path='/Reservas' element={<Reservas />} />
-        <Route path='/Modalidade' element={<Modalidade />} />
-        <Route path='/Disponibilidade' element={<DisponibilidadeEspaco />} />
-        <Route path='/ModalidadeEspaco' element={<ModalidadeEspaco />} />
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
+          <Route path='/Espaco' element={<Espaco />} />
+          <Route path='/Reservas' element={<Reservas />} />
+          <Route path='/Modalidade' element={<Modalidade />} />
+          <Route path='/Disponibilidade' element={<DisponibilidadeEspaco />} />
+          <Route path='/ModalidadeEspaco' element={<ModalidadeEspaco />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
