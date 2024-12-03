2f2badca8fd536b7dcc91f2e6490f1fe73fba7b1
Guilherme Picasso <guipicasso3@gmail.com>
2024-12-02 21:42:23 -0300

    CardModalidade + inputs

--- adm-fired-up/src/Pages/GerenciarEspacos/CriarEspaco.js
+++ adm-fired-up/src/Pages/GerenciarEspacos/CriarEspaco.js
@@ -11,6 +11,9 @@ function CriarEspaco() {
     const [modalidades, setModalidades] = useState([]);
     const [selectedItems, setSelectedItems] = useState([]);
     const [selectedModalidades, setSelectedModalidades] = useState([]);
+    const [nomeEspaco, setNomeEspaco] = useState('');
+    const [imagemEspaco, setImagemEspaco] = useState(null);
+    const [regrasEspaco, setRegrasEspaco] = useState('');
 
     useEffect(() => {
         async function carregarItens() {
@@ -45,12 +48,47 @@ function CriarEspaco() {
         );
     };
 
+    const handleImagemChange = (event) => {
+        setImagemEspaco(event.target.files[0]);
+    };
+
     return (
         <div className="CriarEspaco">
+            <h1>Criar Espaços</h1>
             <div className="content">
+                <section className="inputs">
+                    <div>
+                        <label htmlFor="nomeEspaco">Nome do Espaço</label>
+                        <input className="InputTexto"
+                            type="text"
+                            id="nomeEspaco"
+                            value={nomeEspaco}
+                            onChange={(e) => setNomeEspaco(e.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="imagemEspaco">Imagem do Espaço</label>
+                        <input className="InputImage"
+                            type="file"
+                            id="imagemEspaco"
+                            onChange={handleImagemChange}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="regrasEspaco">Regras do Espaço</label>
+                        <textarea className="AreadeTexto"
+                            id="regrasEspaco"
+                            value={regrasEspaco}
+                            onChange={(e) => setRegrasEspaco(e.target.value)}
+                        />
+                    </div>
+                </section>
                 <section>
                     <div className="cabecalho">
                         <h2>Lista de itens</h2>
+                        <p>*Selecione os itens clicando neles</p>
                     </div>
                     <div className="list">
                         {itens.map((item) => (
@@ -58,27 +96,31 @@ function CriarEspaco() {
                                 key={item.idItem}
                                 item={item}
                                 isClickable={true} // Indica que os cards serão clicáveis
-                                onSelectItem={handleSelectItem} // Passa a função para capturar o ID
-                                isSelected={selectedItems.includes(item.idItem)}
+                                onSelectItem={handleSelectItem} //
+                                isSelected={selectedItems.includes(item.idItem)}//
                             />
                         ))}
                     </div>
                 </section>
-                    <h3>Itens Selecionados</h3>
-                    <p>{selectedItems || "Nenhum item selecionado"}</p>
+
 
                 <section>
                     <div className="cabecalho">
                         <h2>Lista de modalidades</h2>
+                        <p>*Selecione os itens clicando neles</p>
                     </div>
                     <div className="list">
                         {modalidades.map((modalidade) => (
                             <CardModalidade
                                 key={modalidade.idModalidade}
-                                modalidade={modalidade} />
+                                modalidade={modalidade}
+                                isClickable={true}
+                                isSelected={selectedModalidades.includes(modalidade.idModalidade)}
+                                onSelectModalidade={handleSelectModalidade} />
                         ))}
                     </div>
                 </section>
+
             </div>
         </div>
     );
